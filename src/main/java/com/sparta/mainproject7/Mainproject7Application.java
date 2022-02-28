c6d6ab3d57bef8c767e25458287880b21239d1fd
corud1130 <corud1130@gmail.com>
2022-02-28 16:31:44 +0900

    타임스탬프 추가

--- src/main/java/com/sparta/mainproject7/Mainproject7Application.java
+++ src/main/java/com/sparta/mainproject7/Mainproject7Application.java
@@ -2,8 +2,12 @@ package com.sparta.mainproject7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableJpaAuditing
 public class Mainproject7Application {
 
     public static void main(String[] args) {
